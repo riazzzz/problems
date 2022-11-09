df40a087bcf28e03c4168ae2254c351560644be7
riazzzz <32696650+riazzzz@users.noreply.github.com>
2022-11-09 20:54:32 +0600

    Create template.cpp

---
+++ codeforces/template.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PI      acos(-1.0)
+#define all(x) x.begin(),x.end()
+#define nl      '\n' 
+
+
+//////////////code
+
+void solve()
+{
+
+int n;
+cin>>n;
+
+
+for(int i=0;i<n;i++){
+
+
+
+}
+
+
+
+
+
+/////////////////////endcode;
+}
+
+
+
+
+
+int main()
+{
+ //{
+  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+  #ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+
+freopen("output.txt", "w", stdout);
+#else
+// online submission
+#endif
+//}
+
+
+
+int t;
+cin>>t;
+
+while(t--){
+
+
+ //////////////////////////////////codee 
+solve();
+
+
+
+}
+
+
+return 0;
+
+
+}
